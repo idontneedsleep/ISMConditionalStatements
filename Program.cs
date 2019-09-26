733deededea7e0a5db388269d4779e333685d825
idontneedsleep <55160663+idontneedsleep@users.noreply.github.com>
2019-09-27 00:07:50 +0300

    SwitchLoopDoWhile

--- Program.cs
+++ Program.cs
@@ -1,25 +1,155 @@
 ﻿using System;
+using System.Windows;
 
-namespace ForConsole7
+
+namespace SwitchProjectConsole1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            int k = Convert.ToInt32(Console.ReadLine());
-            int t = 1;
-            int s = 0;
-            for (int j = 1; j <= n; j++)
+            int x;
+            do
             {
-                for (int i = 1; i <= k; i++)
+                Console.WriteLine("1.Расшифровка дня недели.");
+            Console.WriteLine("2.Расшифровка месяца.");
+            Console.WriteLine("3.Выполнение операций над числами.");
+            Console.WriteLine("4.Выход");
+                x = int.Parse(Console.ReadLine());
+                switch (x)
                 {
-                    t = t * j;
+                    case 1:
+                        {
+                            Console.WriteLine("ввести число от 1 до 7");
+                            int y = int.Parse(Console.ReadLine());
+                            switch (y)
+                            {
+                                case 1:
+                                    Console.WriteLine("понедельник");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("вторник");
+                                    break;
+                                case 3:
+                                    Console.WriteLine("среда");
+                                    break;
+                                case 4:
+                                    Console.WriteLine("четверг");
+                                    break;
+                                case 5:
+                                    Console.WriteLine("пятница");
+                                    break;
+                                case 6:
+                                    Console.WriteLine("суббота");
+                                    break;
+                                case 7:
+                                    Console.WriteLine("воскресенье");
+                                    break;
+                                default:
+                                    Console.WriteLine("ошибка ввода");
+                                    break;
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("ввести число от 1 до 12");
+                            int y = int.Parse(Console.ReadLine());
+                            switch (y)
+                            {
+                                case 1:
+                                    Console.WriteLine("январь");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("февраль");
+                                    break;
+                                case 3:
+                                    Console.WriteLine("март");
+                                    break;
+                                case 4:
+                                    Console.WriteLine("апрель");
+                                    break;
+                                case 5:
+                                    Console.WriteLine("май");
+                                    break;
+                                case 6:
+                                    Console.WriteLine("июнь");
+                                    break;
+                                case 7:
+                                    Console.WriteLine("июль");
+                                    break;
+                                case 8:
+                                    Console.WriteLine("август");
+                                    break;
+                                case 9:
+                                    Console.WriteLine("сентябрь");
+                                    break;
+                                case 10:
+                                    Console.WriteLine("октябрь");
+                                    break;
+                                case 11:
+                                    Console.WriteLine("ноябрь");
+                                    break;
+                                case 12:
+                                    Console.WriteLine("ноябрь");
+                                    break;
+                                default:
+                                    Console.WriteLine("ошибка ввода");
+                                    break;
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("ввести числа a и b по очередности");
+                            double g;
+                            double a = double.Parse(Console.ReadLine());
+                            double b = double.Parse(Console.ReadLine());
+                            Console.WriteLine("1. Сложение");
+                            Console.WriteLine("2. Умножение");
+                            Console.WriteLine("3. Вычитание");
+                            Console.WriteLine("4. Деление");
+                            int z = int.Parse(Console.ReadLine());
+                            switch (z)
+                            {
+                                case 1:
+                                    {
+                                        g = a + b;
+                                        Console.WriteLine(g);
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        g = a * b;
+                                        Console.WriteLine(g);
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        g = a - b;
+                                        Console.WriteLine(g);
+                                        break;
+                                    }
+                                case 4:
+                                    {
+                                        g = a / b;
+                                        Console.WriteLine(g);
+                                        break;
+                                    }
+                                default:
+                                    Console.WriteLine("ошибка ввода");
+                                    break;
+                            }
+                            break;
+                        }
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("ошибка ввода");
+                        break;
                 }
-                s = s + t;
-                t = 1;
-            }
-            Console.WriteLine(s);
+            } while (x != 4);
         }
     }
 }
